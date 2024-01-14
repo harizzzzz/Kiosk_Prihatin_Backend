e32c8e14dfa5e69f66b32aebf0ba5ada0e00613e
Hariz Zamzuri <harizzamzuri20@gmail.com>
2024-01-14 12:27:39 +0800

    some changes donno

--- src/reserve/reserve.controller.ts
+++ src/reserve/reserve.controller.ts
@@ -19,4 +19,19 @@ export class ReserveController {
   async create(@Body() createReserveDto: CreateReserveDto) {
     return await this.reserveService.createReservation(createReserveDto);
   }
+
+  @Get('getOneSession/:session_id')
+  async getOneSession(@Param('session_id') session_id: string) {
+    return await this.reserveService.getOneSession(session_id);
+  }
+
+  @Get('sessionExist/:session_id')
+  async getExisted(@Param('session_id') session_id: string) {
+    return await this.reserveService.getSessionExist(session_id);
+  }
+
+  @Get('reserveHistory/:student_id')
+  async getReserveHistory(@Param('student_id') student_id: string) {
+    return await this.reserveService.retrieveHistory(student_id);
+  }
 }
--- src/reserve/reserve.service.ts
+++ src/reserve/reserve.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { CreateReserveDto } from './dto/create-reserve.dto';
 import { UpdateReserveDto } from './dto/update-reserve.dto';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
@@ -55,6 +55,100 @@ export class ReserveService {
       }
     } catch (error) {
       console.log(error);
+      throw new BadRequestException();
+    }
+  }
+
+  async retrieveHistory(student_id: string) {
+    try {
+      const distinctReserveQuery = await this.reserveRepo
+        .createQueryBuilder('reserve')
+        .select('reserve.*')
+        .where('reserve.student_id IN (:student_id)', {
+          student_id: student_id,
+        });
+
+      const distinctReserveCount = await distinctReserveQuery.getCount();
+
+      const query = this.reserveRepo
+        .createQueryBuilder('reserve')
+        .select([
+          'reserve.reserve_id as reserve_id',
+          'reserve.session_id as session_id',
+          `DATE_FORMAT(reserve.reserve_time, '%d/%m/%Y, %h:%i %p') AS reserve_time`,
+          'ROW_NUMBER() OVER (PARTITION BY reserve.session_id ORDER BY reserve.reserve_time) AS RowNum',
+        ])
+        .orderBy('4')
+        .limit(distinctReserveCount);
+
+      const reserveHistory = await query.getRawMany();
+
+      return reserveHistory;
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async getOneSession(session_id: string) {
+    try {
+      const sesh = await this.getSessionExist(session_id);
+      if (sesh) {
+        const query = this.reserveRepo
+          .createQueryBuilder('reserve')
+          .select([
+            'reserve.reserve_id as reserve_id',
+            'reserve.student_id as student_id',
+            'users.full_name as student_name',
+            'reserve.item_id as item_id',
+            'item.item_name as item_name',
+            'reserve.quantity as item_quantity',
+            'reserve.reserve_time as time_created',
+          ])
+          .leftJoin('reserve.student', 'users')
+          .leftJoin('reserve.item', 'item')
+          .where('reserve.session_id IN (:session_id)', {
+            session_id: session_id,
+          });
+        try {
+          const [sql, parameters] = query.getQueryAndParameters();
+
+          // Log the SQL query and parameters
+          Logger.log(`SQL Query: ${sql}`);
+          Logger.log(`Parameters: ${JSON.stringify(parameters)}`);
+          const oneSession = await query.getRawMany();
+          return oneSession;
+        } catch (err) {
+          console.log(err);
+          throw new BadRequestException();
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async getSessionExist(session_id: string) {
+    try {
+      const query = this.reserveRepo
+        .createQueryBuilder('reserve')
+        .select(['reserve.reserve_id'])
+        .where('reserve.session_id=:session_id', { session_id: session_id });
+      try {
+        const res = await query.getExists();
+
+        if (res) {
+          return res;
+        } else {
+          return { message: 'session does not exist !' };
+        }
+      } catch (e) {
+        console.log(e);
+        throw new BadRequestException();
+      }
+    } catch (e) {
+      console.log(e);
       throw new BadRequestException();
     }
   }
